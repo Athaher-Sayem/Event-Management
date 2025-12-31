2479150a47085e81ef19a8ca6a8907248a51c831
Athaher Sayem <athahersayem@outlook.com>
2026-01-01 02:25:53 +0600

    All UI with responsive Done without extend

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from events.views import Home_view
+from events.views import Home_view,Today_view,Upcomming_view,Past_view,About_view
 
 urlpatterns = [
     path('Home/', Home_view),
+    path('Today_Events/', Today_view),
+    path('Upcomming_Events/',Upcomming_view),
+    path('Past_Events/',Past_view),
+    path('About_Us/',About_view),
    
 ]
--- events/views.py
+++ events/views.py
@@ -5,3 +5,21 @@ def Home_view(request):
     return render(request,"Home.html")
 
 
+def Today_view(request):
+    return render(request,"Today.html")
+
+
+
+def Upcomming_view(request):
+    return render(request,"Upcomming.html")
+
+
+
+
+def Past_view(request):
+    return render(request,"Past.html")
+
+
+
+def About_view(request):
+    return render(request,"About.html")
