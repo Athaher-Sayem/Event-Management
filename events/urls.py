e9065becb1cf211bba93c9134702942be2b9dc3c
Athaher Sayem <athahersayem@outlook.com>
2026-01-01 19:38:26 +0600

    Added Dynamic url and NAV bar

--- events/urls.py
+++ events/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from events.views import Home_view,Today_view,Upcomming_view,Past_view,About_view,Create_Event,Participant_Reg
+from events.views import Home_view, Today_view, Upcomming_view, Past_view, About_view, Create_Event, Participant_Reg
 
 urlpatterns = [
-    path('Home/', Home_view),
-    path('Today_Events/', Today_view),
-    path('Upcomming_Events/',Upcomming_view),
-    path('Past_Events/',Past_view),
-    path('About_Us/',About_view),
-    path('Create_Event/',Create_Event, name='Create_Event'),
-    path('Participant_Reg/',Participant_Reg, name='Participant_Reg'),
-   
+    path('Home/', Home_view, name='home'),
+    path('Today_Events/', Today_view, name='today_events'),
+    path('Upcomming_Events/', Upcomming_view, name='upcoming_events'),
+    path('Past_Events/', Past_view, name='past_events'),
+    path('About_Us/', About_view, name='about_us'),
+    path('Create_Event/', Create_Event, name='Create_Event'),
+    path('Participant_Reg/', Participant_Reg, name='Participant_Reg'),
 ]
