a8079ce875aa1c39a5893c1e2d732fc983b3fdf4
Athaher Sayem <athahersayem@outlook.com>
2026-01-03 14:54:14 +0600

    Ready to deploy v3

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from events.views import *
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('Home/', Home_view, name='home'),
@@ -14,4 +15,5 @@ urlpatterns = [
     path('Participant_Event/<int:event_id>/', event_participant_view, name='event_participant_view'),
     path('Update_View/<int:event_id>/', Update_view, name='update_view'),
     path('search/', Search_event_view, name='Search_event_view'),
+    path('', RedirectView.as_view(url='events/')),
 ]
