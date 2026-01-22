7ca245304d534bc745464c29f744628b6656c55d
Athaher Sayem <athahersayem@outlook.com>
2026-01-22 18:03:17 +0600

    Ready to deploy v1

--- event_management/settings.py
+++ event_management/settings.py
@@ -152,7 +152,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 # These will now pull from the .env file
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 SITE_URL = config('SITE_URL', default='http://127.0.0.1:8000')
+
+
